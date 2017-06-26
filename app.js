9345c8c759326b4800a8f3a2f9948fc4b0fece37
Robert Hutchins <rjhutchins22@gmail.com>
2017-06-26 14:19:47 -0400

    finish easy mode of project

--- app.js
+++ app.js
@@ -4,3 +4,52 @@ const session = require('express-session');
 const mustacheExpress = require('mustache-express');
 const data = require('./data.js');
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.engine('mustache', mustacheExpress());
+app.set('views', ['./views','./login']);
+app.set('view engine', 'mustache');
+
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+
+// app.use('/', function(req, res, next){
+//   let username = req.body.username;
+//   let password = req.body.password;
+//
+//   data.Auth(req, username, password);
+//   console.log("username: " + username + "password: " +password)
+//   console.log(req.sessions);
+// });
+
+app.get('/', function(req, res){
+  res.redirect('/login');
+});
+
+app.get('/login', function(req, res){
+  res.render('login');
+})
+
+app.post('/', function(req,res){
+  let username = req.body.username;
+  let password = req.body.password;
+
+  data.Auth(req, username, password);
+  console.log(req.session)
+  if(req.session && req.session.authenticated){
+    res.render('index', { user: username})
+  }
+  else{
+    res.redirect('/login');
+  }
+});
+
+app.listen(3000, function(req, res){
+  console.log('looks like you made it after all.')
+})
--- data.js
+++ data.js
@@ -13,5 +13,27 @@ const users = [
 
 function getUser(username)
 {
-  
+  return users.username.find(function(user){
+    return user.username === username;
+  });
+}
+
+function Authenticate(req, username, password){
+  let authenticatedUser = users.find(function(user){
+    if(user.username === username && user.password === password){
+      console.log('true');
+      req.session.authenticated = true;
+    }
+    else{
+      return false
+    }
+  });
+  return req.session
+}
+
+
+module.exports = {
+  getUsr : getUser,
+  getAllUsers : users,
+  Auth : Authenticate
 }
